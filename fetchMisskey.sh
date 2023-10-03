674db8ec7238fa8904bd323145e5b63f65a672af
Fine Archs <cicada1003@gmail.com>
2023-10-04 00:19:53 +0900

    fix

--- fetchMisskey.sh
+++ fetchMisskey.sh
@@ -1,9 +1,11 @@
 REPOPREFIX='https://raw.githubusercontent.com/misskey-dev/misskey/develop/'
 PREFIX='./src/misskey/'
 while read -r input; do
-	if [[ $input =~ '#' ]]; then
-		:
-	elif [[ $input =~ '\t' ]]; then
+	echo $(printf %.1s $input)
+	if [ $(printf %.1s $input) = '#' ]; then
+		echo $input
+	elif [ $(printf %.1s $input) = $'\t' ]; then
+		echo $input
 		curl $REPOPREFIX$path$input -o $PREFIX$path$input
 	else
 		path=$input
