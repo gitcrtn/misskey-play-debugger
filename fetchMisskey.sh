af06adf26d6fa36076f610283577bfbdd016c5ce
Fine Archs <cicada1003@gmail.com>
2023-10-03 23:18:38 +0900

    new fetcher

--- fetchMisskey.sh
+++ fetchMisskey.sh
@@ -1,3 +1,11 @@
-curl \
-	https://raw.githubusercontent.com/misskey-dev/misskey/develop/packages/frontend/src/scripts/aiscript/ui.ts \
-	-o ./src/misskey/scripts/aiscript/ui.ts
+REPOPREFIX='https://raw.githubusercontent.com/misskey-dev/misskey/develop/'
+PREFIX='./src/misskey/'
+while read -r input; do
+	if [[ $input =~ '#' ]]; then
+		:
+	elif [[ $input =~ '\t' ]]; then
+		curl $REPOPREFIX$path$input -o $PREFIX$path$input
+	else
+		path=$input
+	fi
+done < "files-misskey"
